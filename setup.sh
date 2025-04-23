365ee12a4ccfb4738e136947d9035b3c69b891e8
vuqn1996 <35033011+vuqn1996@users.noreply.github.com>
2025-04-23 17:06:36 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,10 +7,12 @@ PROJECT_DIR="/home/$USER/jetson-fan-web"
 # Step 1: Update system and install dependencies
 sudo apt update
 sudo apt install -y python3-pip python3-tk
-
-# Step 2: Install Flask
+# Step 2: Change target_pwm owner and chmod
+sudo chown asilla:asilla /sys/devices/pwm-fan/target_pwm
+sudo chmod 666 /sys/devices/pwm-fan/target_pwm
+# Step 3: Install Flask
 pip3 install flask psutil
-# Step 3: Create systemd service file
+# Step 4: Create systemd service file
 cat <<EOL | sudo tee /etc/systemd/system/jetson-fan-web.service
 [Unit]
 Description=Jetson Fan Control Web Dashboard
@@ -28,10 +30,10 @@ Environment=PYTHONUNBUFFERED=1
 WantedBy=multi-user.target
 EOL
 
-# Step 4: Enable and start the service
+# Step 5: Enable and start the service
 sudo systemctl daemon-reload
 sudo systemctl enable jetson-fan-web
 sudo systemctl start jetson-fan-web
 
-# Step 5: Confirm the service is running
+# Step 6: Confirm the service is running
 sudo systemctl status jetson-fan-web
